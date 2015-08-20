520c088f447bf3eebd52653ae17ebb308ee663dd
ryancmoon <ryan@organizedvillainy.com>
2015-08-20 15:21:07 -0500

    Create user_add.sh

---
+++ bash/user_add.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# user_add.sh
+# Ryan C. Moon
+# 2012-09-12
+# Creates a new user in ubuntu with the ssh key setup below (optional).
+# usage: ./user_add ryan
+
+USER="$1"
+KEY=""
+
+useradd $USER -s /bin/bash -m
+passwd $USER
+mkdir -p /home/$USER 
+chown $USER:$USER /home/$USER 
+mkdir /home/$USER/.ssh
+chmod 700 /home/$USER/.ssh
+echo "$KEY" >> /home/$USER/.ssh/authorized_keys
+chmod 600 /home/$USER/.ssh/authorized_keys
+chown -R $USER:$USER /home/$USER
