2f01ced84d3f85d8d2c2343e18320fb305655314
ryancmoon <ryan@organizedvillainy.com>
2015-09-23 16:34:55 -0500

    Create spamassassin_autogenerate_whitelist.sh

---
+++ bash/spamassassin_autogenerate_whitelist.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# spamassassin_autogenerate_whitelist.sh
+# Ryan C. Moon
+# 2015-09-23
+# Autogenerates a whitelist for SpamAssassin based on all your senders.
+# REQUIRES Postfix setup to use MAILDIR
+# REQUIRES SpamAssassin to include the CUSTOM_WHITELIST file in local.cf. 
+
+CUSTOM_WHITELIST=/etc/spamassassin/auto_whitelist.cf
+
+for x in $(ls /home); do find /home/$x/Maildir/.Sent/ -type f -exec grep -Eo "^To:.*$" {} \; | sed 's/To:\ //g' | grep -Eo "[A-Za-z0-9]+@[^.]+\.[a-z]{2,6}" | sort -u; done | sort -u > /tmp/senders.txt
+
+echo "Whitelist has : $(wc -l /tmp/senders.txt) senders.."
+
+for x in $(cat /tmp/senders.txt); do echo "whitelist_to $x" >> $CUSTOM_WHITELIST; echo "whitelist_from $x" >> $CUSTOM_WHITELIST; done
+
+echo "Restarting Spamassassin.."
+service spamassassin restart
+
+echo "Done.."
